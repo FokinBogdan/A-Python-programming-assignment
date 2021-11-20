3dc7e0890ab42046fe019f212fe60d79a6b47c08
FokinBogdan <fokin.boghdan@mail.ru>
2021-11-20 18:49:09 +0500

    Пункты №5-8

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # A-Python-programming-assignment
+Пункты 5-8
+
+Изображение до: ![ball.jpg](ball.jpg)
+
+Изображение после работы filter.py : ![new ball.jpg](new ball.jpg)
+
+Изображение после работы old_filter.py: ![res.jpg](res.jpg)
+
+Время выполнения filter.py: ![img.png](img.png)
+
+Время выполнения old_filter: ![img_1.png](img_1.png)
+
+Время выполнения filter.py больше чем old_filter.py, потому что при работе с filter.py мы вручную вводили "Название загружаемого изображения", "Размер мозаики", "Шаг серого" и "Название выгружаемого изображения", а это занимает время работы filter.py. Поэтому, время работы filter.py зависит от быстродействия пользователя: чем быстрее пользователь введёт входные данные, тем быстрее выполнится filter.py.
+
+Время выполнения filter_with_filename.py: ![img_2.png](img_2.png)
+
+Время выполнения filter_with_filename.py меньше чем filter.py, потому что данные инициализируются автоматически. Также filter_with_filename выполняется быстрее old_filter, так как filter_with_filename - это усовершенствованный old_filter, и old_filter работает некорректно.  
---
+++ filter_with_filename.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import numpy as np
+
+def get_average(arr):
+    return int(np.sum(arr) / 3 // (mosaic_size ** 2))
+
+
+def draw_mosaic_pixel(color):
+    for a in range(i, i + mosaic_size):
+        for b in range(j, j + mosaic_size):
+            img_array[a][b] = [color, color, color]
+
+
+print('Введите название загружаемого изображения:')
+input_image_name = "ball.jpg"
+
+img = Image.open(input_image_name)
+img_array = np.array(img)
+width, height = img.size
+
+print('Введите размер мозаики:')
+mosaic_size = 10
+print('Введите шаг серого:')
+graduation = 50
+
+#растягиваю или сжимаю изображение, чтобы оно соответсвовало целому числу пикселей мозайки
+if width % mosaic_size != 0 or height % mosaic_size != 0:
+    width = round(width / mosaic_size) * mosaic_size
+    height = round(height / mosaic_size) * mosaic_size
+    img_array = np.array(img.resize((width, height)))
+
+
+for i in range(0, height, mosaic_size):
+    for j in range(0, width, mosaic_size):
+        average = get_average(img_array[i:i + mosaic_size, j:j + mosaic_size])
+        draw_mosaic_pixel(int(average // graduation) * graduation)
+
+print('Введите название выгружаемого изображения:')
+output_image_name = "new ball2.jpg"
+
+res = Image.fromarray(img_array)
+res.save(output_image_name)
