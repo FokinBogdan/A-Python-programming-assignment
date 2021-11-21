b12bb9af7de0ee12c88a2022e4fc4afeb221822d
FokinBogdan <fokin.boghdan@mail.ru>
2021-11-21 17:02:59 +0500

    Пункты №9-11

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # A-Python-programming-assignment
-Пункты 5-8
 
 Изображение до: ![ball.jpg](ball.jpg)
 
@@ -15,4 +14,8 @@
 
 Время выполнения filter_with_filename.py: ![img_2.png](img_2.png)
 
-Время выполнения filter_with_filename.py меньше чем filter.py, потому что данные инициализируются автоматически. Также filter_with_filename выполняется быстрее old_filter, так как filter_with_filename - это усовершенствованный old_filter, и old_filter работает некорректно.  
+Время выполнения filter_with_filename.py меньше чем filter.py, потому что данные инициализируются автоматически. Также filter_with_filename выполняется быстрее old_filter, так как filter_with_filename - это усовершенствованный old_filter, и old_filter работает некорректно.
+
+Скриншот выполненных doc-тестов: ![img_3.png](img_3.png)
+Смог реализовать doc-тесты только для функции "get_average", которая возвращает среднее значение сумм цветов каждого пикселя на участве arr с шагом в mosaic_size.
+Для функции "draw_mosaic_pixel" doc-тесты не реализовал.
--- filter.py
+++ filter.py
@@ -1,15 +1,42 @@
 from PIL import Image
 import numpy as np
+import doctest
 
-def get_average(arr):
-    return int(np.sum(arr) / 3 // (mosaic_size ** 2))
+
+def get_average(arr, size_mosaic):
+    """
+    Возвращает среднее значение сумм цветов каждого пикселя на участве arr с шагом в mosaic_size
+    Параметры:
+    :param arr: участок
+    :param size_mosaic: шаг серого
+    :return: среднее значение цвета пикселей на участке
+    doctest:
+    >>> get_average([[[1, 2, 3]]], 1)
+    2
+    >>> get_average([[[1, 2, 3]] * 3] * 3, 3)
+    2
+    >>> get_average([[[0, 0, 0]]], 1)
+    0
+
+    Чтобы реализовать данные тесты, get_average принимает size_mosaic
+    """
+    return int(np.sum(arr) / 3 // (size_mosaic ** 2))
 
 
 def draw_mosaic_pixel(color):
+    """
+    Рисует мозаику поверх старого изображения
+    Параметры:
+    :param color: среднее значение цвета пикселей этого участка
+    doctest:
+    Не смог реализовать тесты для данной функции
+    """
     for a in range(i, i + mosaic_size):
         for b in range(j, j + mosaic_size):
             img_array[a][b] = [color, color, color]
 
+
+print(doctest.testmod())
 
 print('Введите название загружаемого изображения:')
 input_image_name = input()
@@ -23,16 +50,15 @@ mosaic_size = int(input())
 print('Введите шаг серого:')
 graduation = int(input())
 
-#растягиваю или сжимаю изображение, чтобы оно соответсвовало целому числу пикселей мозайки
+# растягиваю или сжимаю изображение, чтобы оно соответсвовало целому числу пикселей мозайки
 if width % mosaic_size != 0 or height % mosaic_size != 0:
     width = round(width / mosaic_size) * mosaic_size
     height = round(height / mosaic_size) * mosaic_size
     img_array = np.array(img.resize((width, height)))
 
-
 for i in range(0, height, mosaic_size):
     for j in range(0, width, mosaic_size):
-        average = get_average(img_array[i:i + mosaic_size, j:j + mosaic_size])
+        average = get_average(img_array[i:i + mosaic_size, j:j + mosaic_size], mosaic_size)
         draw_mosaic_pixel(int(average // graduation) * graduation)
 
 print('Введите название выгружаемого изображения:')
